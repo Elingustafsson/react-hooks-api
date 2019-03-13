82681870c5bd9df4a9fb2176556fcb7fa7cc2be7
Gustafsson Elin (Ext) <ext.elin.gustafsson@tieto.com>
2019-03-13 17:00:30 +0100

    sorted comments in postscreen

--- react-hooks-api/src/PostScreen.js
+++ react-hooks-api/src/PostScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default function PostScreen(props) {
-  const [state, setState] = useState({})
+  const [state, setState] = useState(undefined)
 
 
   useEffect(() => {
@@ -10,17 +10,25 @@ export default function PostScreen(props) {
     fetch("http://5c63e54bc969210014a32d76.mockapi.io/api/v1/users/" + props.match.params.userId + "/posts/" + props.match.params.postId)
     .then(promise => promise.json())
     .then(data => {
-      setState({data: data})
+      data.comments.sort((a, b) => {
+        if(a.createdAt < b.createdAt) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+      setState(data)
       document.body.classList.remove('loading')
     })
   }, [])
 
 
+
   function mapComments() {
-   if(state.data === undefined) {
+   if(state === undefined) {
      return null;
    }
-   const comments = state.data.comments.map(comment => (
+   const comments = state.comments.map(comment => (
      <div key={comment.id}>
        <p>{comment.name}</p>
        <p>{comment.text}</p>
@@ -30,13 +38,13 @@ export default function PostScreen(props) {
    ))
    return (
     <div>
-    <h1>Enskild post</h1>
-      <p>Skapad: {state.data.createdAt}</p>
-      <p>Antal likes: {state.data.likes}</p>
-      <h2>Titel: {state.data.title}</h2>
-      <h3>{state.data.text}</h3>
-      <h1>Kommentarer</h1>
-      {comments}
+      <h1>Enskild post</h1>
+        <p>Skapad: {state.createdAt}</p>
+        <p>Antal likes: {state.likes}</p>
+        <h2>Titel: {state.title}</h2>
+        <h3>{state.text}</h3>
+        <h1>Kommentarer</h1>
+        {comments}
     </div>
    )
  }
